fd0f837351088675e0aaf983f507f31d6561b65e
Adriano <adriano.selaviles@gmail.com>
2019-02-03 13:21:03 -0800

    ll

---
+++ datastructures/linkedlist/linkedlist.go
@@ -0,0 +1,25 @@
+package linkedlist
+
+// LList represents a linked list
+type LList struct {
+	Head Node
+	Tail Node
+}
+
+// DLList represents a doubly linked list
+type DLList struct {
+	Head Node
+	Tail Node
+}
+
+// CLList represents a circular linked list
+type CLList struct {
+	Head Node
+	Tail Node
+}
+
+// CDLList represents a circular doubly linked list
+type CDLList struct {
+	Head Node
+	Tail Node
+}
---
+++ datastructures/linkedlist/node.go
@@ -0,0 +1,39 @@
+package linkedlist
+
+import "fmt"
+
+// Node is a generic "node" e.g. for singly linked lists
+type Node struct {
+	Data interface{}
+	Next *Node
+}
+
+// PNode adds a "previous" pointer to Node to allow doubly linked lists
+type PNode struct {
+	Node
+	Prev *PNode
+}
+
+// WalkForward moves k steps along the next edge of nodes in a list
+func (n *Node) WalkForward(k int) (*Node, error) {
+	for i := 0; i < k; i++ {
+		if n.Next != nil {
+			n = n.Next
+		} else {
+			return nil, fmt.Errorf("list not long enough, found nil after %d steps", i)
+		}
+	}
+	return n, nil
+}
+
+// WalkBackward moves k steps along the prev edge of nodes in a list
+func (n *PNode) WalkBackward(k int) (*PNode, error) {
+	for i := 0; i < k; i++ {
+		if n.Prev != nil {
+			n = n.Prev
+		} else {
+			return nil, fmt.Errorf("list not long enough, found nil after %d steps", i)
+		}
+	}
+	return n, nil
+}
