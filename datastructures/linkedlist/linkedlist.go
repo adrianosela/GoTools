3598d8dbee82719026945c432d4d8ffa50c148f8
Adriano Sela Aviles <adriano.selaviles@gmail.com>
2019-02-03 14:41:04 -0800

    Update linkedlist.go

--- datastructures/linkedlist/linkedlist.go
+++ datastructures/linkedlist/linkedlist.go
@@ -2,24 +2,24 @@ package linkedlist
 
 // LList represents a linked list
 type LList struct {
-	Head Node
-	Tail Node
+	Head *Node
+	Tail *Node
 }
 
 // DLList represents a doubly linked list
 type DLList struct {
-	Head Node
-	Tail Node
+	Head *Node
+	Tail *Node
 }
 
 // CLList represents a circular linked list
 type CLList struct {
-	Head Node
-	Tail Node
+	Head *Node
+	Tail *Node
 }
 
 // CDLList represents a circular doubly linked list
 type CDLList struct {
-	Head Node
-	Tail Node
+	Head *Node
+	Tail *Node
 }
